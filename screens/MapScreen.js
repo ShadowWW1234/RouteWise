82eff55c11e60e1333d50827b144f4ce0b25ade8
shadow <harrenantazo25@gmail.com>
2024-09-21 16:04:31 +0800

    update mapscreen user location

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,49 +1,101 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View, PermissionsAndroid } from 'react-native';
+import React, { useEffect } from 'react';
 import MapboxGL from '@rnmapbox/maps';
+import Geolocation from '@react-native-community/geolocation';
+import { useDispatch, useSelector } from 'react-redux';
+import { setOrigin, selectOrigin } from '../slices/navSlice';
 
 MapboxGL.setAccessToken('pk.eyJ1Ijoic2hhZG93MjI2IiwiYSI6ImNtMTl6d3NnaDFrcWIyanM4M3pwMTYxeDQifQ.wDv2IuFGRpUASw1jx540Ng');
 
 const MapScreen = () => {
+  const dispatch = useDispatch();
+  const origin = useSelector(selectOrigin);
+
+  const requestLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Location Permission',
+          message: 'App needs access to your location.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        }
+      );
+
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        getLocation(); // Fetch location if permission granted
+      } else {
+        console.log('Location permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const getLocation = () => {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        const { longitude, latitude } = position.coords;
+        dispatch(setOrigin({
+          location: { latitude, longitude },
+          description: 'Your current location',
+        }));
+      },
+      (error) => {
+        console.error('Error getting location:', error);
+      },
+      { enableHighAccuracy: true, timeout: 60000, maximumAge: 1000 }
+    );
+  };
+
+  useEffect(() => {
+    requestLocationPermission();
+  }, [dispatch]);
+
   return (
     <View style={styles.container}>
-    <MapboxGL.MapView
-      style={styles.map}
-      zoomEnabled={true}
-      styleURL='mapbox://styles/mapbox/streets-v12'
-      rotateEnabled={true}
-    >
-      <MapboxGL.Camera 
-        zoomLevel={15}
-        centerCoordinate={[120.90245067681445, 14.486578075433032]} // Note: order is [longitude, latitude]
-        pitch={60}
-        animationMode={'flyTo'}
-        animationDuration={6000}
-      />
-      <MapboxGL.PointAnnotation 
-        id='marker'
-        coordinate={[120.90245067681445, 14.486578075433032]} // Note: order is [longitude, latitude]
+      <MapboxGL.MapView
+        style={styles.map}
+        zoomEnabled={true}
+        styleURL="mapbox://styles/mapbox/streets-v12"
+        rotateEnabled={true}
       >
-        <View style={styles.marker} />
-      </MapboxGL.PointAnnotation> 
-    </MapboxGL.MapView>
-  </View>
-  )
-}
+        <MapboxGL.Camera
+          zoomLevel={15}
+          centerCoordinate={origin.location ? [origin.location.longitude, origin.location.latitude] : [120.89943929038361, 14.488862043596518]} // Fallback to static location
+          pitch={60}
+          animationMode={'flyTo'}
+          animationDuration={6000}
+        />
+        
+        <MapboxGL.UserLocation
+           androidRenderMode="gps"
+           visible={true}
+           showsUserHeadingIndicator={true} // Show heading indicator
+          onUpdate={(location) => {
+            dispatch(setOrigin({
+              location: { latitude: location.coords.latitude, longitude: location.coords.longitude },
+              description: 'Your current location',
+            }));
+          }}
+        />
+        
+     
+      </MapboxGL.MapView>
+    </View>
+  );
+};
 
-export default MapScreen
+export default MapScreen;
 
-const styles = StyleSheet.create({container: {
-    flex:1,
-   },
-   map: {
-     flex: 1,
-   },
-   marker: {
-     height: 30,
-     width: 30,
-     backgroundColor: '#f00',
-     borderRadius: 15,
-     borderWidth: 2,
-     borderColor: '#fff',
-   },})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+ 
+});
